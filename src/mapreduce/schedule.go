39aa1f15abe23668ac1106849935505329d6d2a7
Bingfei <tonycao96@gmail.com>
2016-02-05 01:42:45 -0500

    Fix concurrency errors in schedule

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -5,40 +5,6 @@ import (
 	"sync"
 )
 
-type SafeArray struct {
-	arr  []int
-	lock sync.Mutex
-}
-
-func (a SafeArray) length() int {
-	a.lock.Lock()
-	defer a.lock.Unlock()
-	return len(a.arr)
-}
-
-func (a SafeArray) getNext() int {
-	a.lock.Lock()
-	defer a.lock.Unlock()
-	return a.arr[0]
-}
-
-func (a *SafeArray) deleteValue(v int) {
-	a.lock.Lock()
-	for i := 0; i < len(a.arr); i++ {
-		if v == a.arr[i] {
-			a.arr = append(a.arr[:i], a.arr[i+1:]...)
-			break
-		}
-	}
-	a.lock.Unlock()
-}
-
-func (a *SafeArray) addValue(v int) {
-	a.lock.Lock()
-	a.arr = append(a.arr, v)
-	a.lock.Unlock()
-}
-
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
@@ -59,39 +25,32 @@ func (mr *Master) schedule(phase jobPhase) {
 	// Remember that workers may fail, and that any given worker may finish
 	// multiple tasks.
 	//
-	tasks := SafeArray{arr: make([]int, ntasks, 2*ntasks)}
-	for i := 0; i < ntasks; i++ {
-		tasks.arr[i] = i
-	}
+	successes := 0
+	taskChan := make(chan int)
+	lock := &sync.Mutex{}
 
-	/*taskChan := make(chan int)
 	go func() {
 		for i := 0; i < ntasks; i++ {
 			taskChan <- i
 		}
 	}()
-	successes := 0
-	for successes < ntasks {*/
-	for tasks.length() > 0 {
-		next := tasks.getNext()
-		tasks.deleteValue(next)
-		/**		next := <-taskChan
-		if next == -1 {
-			successes++
-			continue
-		} **/
+
+	for next := range taskChan {
 		freeWorker := <-mr.registerChannel
-		taskNum := next
-		go func() {
+		go func(taskNum int) {
 			task := DoTaskArgs{mr.jobName, mr.files[taskNum], phase, taskNum, nios}
 			if call(freeWorker, "Worker.DoTask", &task, new(struct{})) {
-				//taskChan <- -1
-				mr.registerChannel <- freeWorker
+				lock.Lock()
+				successes++
+				if successes == ntasks {
+					close(taskChan)
+				}
+				lock.Unlock()
 			} else {
-				//taskChan <- taskNum
-				tasks.addValue(taskNum)
+				taskChan <- taskNum
 			}
-		}()
+			mr.registerChannel <- freeWorker
+		}(next)
 	}
 
 	fmt.Printf("Schedule: %v phase done\n", phase)
